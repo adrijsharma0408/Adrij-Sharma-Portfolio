0ad75dfdbb0bfdab9661fb63d211dedda01ab34e
AdrijSharma <adrij0408@gmail.com>
2021-08-26 23:53:35 +0530

    Added the .js files containing the code for accomplishments and their styles(user interface)

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 20, text: 'Computer Science Courses'},
+  { number: 5, text: 'Praogramming Languages', },
+  { number: 800, text: 'Instagram Followers', },
+  { number: 150, text: 'Linked-In Followers', },
+  { number: 1, text: 'Best Table Tennis Player in the world' }
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider/>
+  </Section>
+);
+
+export default Acomplishments;
